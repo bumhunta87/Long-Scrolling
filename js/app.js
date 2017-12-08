a669c9f0a4181eec14eb5c3a45759714fcf123e2
bumhunta87 <32181277+bumhunta87@users.noreply.github.com>
2017-12-07 23:09:15 -0800

    Final
    Final

--- js/app.js
+++ js/app.js
@@ -1,32 +1,69 @@
-var canvas = document.getElementById("canvas");
-var counter = document.getElementById("counter");
-var icon = document.getElementById("icon");
+// var canvas = document.getElementById("canvas");
+// var counter = document.getElementById("counter");
+// var icon = document.getElementById("icon");
+
+// window.onscroll = function(){
+
+
+//   var y = window.pageYOffset;
+
+//   console.log(y);
+
+//   counter.style.top = 400 + y * .97 + "px";
+
+//   counter.innerHTML = y + "px";
+
+//   if (y > 2000) {
+//     counter.style.backgroundColor = "green";
+//   }else{
+//     counter.style.backgroundColor = "blue";}
+
+//     icon.style.top = 200 + y  + "px";
+
+//   if(y > 2000){
+
+//       icon.src="http://content.internetvideoarchive.com/content/photos/9249/337362_014.jpg";
+
+//   }else{
+
+//   icon.src="http://www.syfy.com/sites/syfy/files/2017/10/rick_and_morty_1.jpg";
+//  }
+
+// };
+
+
+var maze = document.getElementById("maze");
+var nug = document.getElementById("nug");
+var lighter = document.getElementById("lighter");
 
 window.onscroll = function(){
 
 
-  var y = window.pageYOffset;
+  var x = window.pageXOffset;
 
-  console.log(y);
+  console.log(x);
 
-  counter.style.top = 400 + y * .97 + "px";
+  nug.style.left = 450 + x * .98 + "px";
 
-  counter.innerHTML = y + "px";
+  lighter1.style.left = x * .05 + "px";
+  // lighter2.style.left = x + .3 + "px";
+  // lighter3.style.left = x + .2 + "px";
+  // lighter4.style.left = x + .1 + "px";
 
-  if (y > 2000) {
-    counter.style.backgroundColor = "green";
-  }else{
-    counter.style.backgroundColor = "blue";}
+  // if (x > 200) {
+  //   counter.style.backgroundColor = "green";
+  // }else{
+  //   counter.style.backgroundColor = "blue";}
+  //
+  //   icon.style.left =100+ x;
 
-    icon.style.top = 200 + y  + "px";
-
-  if(y > 2000){
-
-      icon.src="http://content.internetvideoarchive.com/content/photos/9249/337362_014.jpg";
-
-  }else{
-
-  icon.src="http://www.syfy.com/sites/syfy/files/2017/10/rick_and_morty_1.jpg";
- }
+ //  if(x > 200){
+ //
+ //      icon.src="http://content.internetvideoarchive.com/content/photos/9249/337362_014.jpg";
+ //
+ //  }else{
+ //
+ //  icon.src="http://www.syfy.com/sites/syfy/files/2017/10/rick_and_morty_1.jpg";
+ // }
 
 };
